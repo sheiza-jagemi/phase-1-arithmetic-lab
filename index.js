74883c323fdeb4ad72c0e2a820d1017659206ceb
sheiza-jagemi <sheiza.jagemi@student.moringaschool.com>
2025-06-07 12:45:45 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Challenge 1: Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2: Random Integer (greater than 0)
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Challenge 3: Mod (remainder of 4)
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Challenge 4: Max (highest number is 20)
+const max = Math.max(5, 10, 15, 20);
